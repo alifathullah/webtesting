f0395c0d342e5f533287921a44bd9b5ef3a83fc9
Ali Fathullah <email_github_kamu@email.com>
2026-03-10 14:19:08 +0700

    Serve API via json-server and static fallback

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
+const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const jsonServer = require("json-server");
-const path = require("path");
 
-const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({
-  static: false,
-});
+const app = express();
 const PORT = process.env.PORT || 3000;
+const root = path.resolve(__dirname);
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 
 app.use(cors());
-app.use(middlewares);
+app.use(express.static(root));
 app.use(jsonServer.bodyParser);
 app.use(router);
+
+app.get("*", (req, res) => {
+  if (req.method === "GET" && req.accepts("html")) {
+    res.sendFile(path.join(root, "login.html"));
+  } else {
+    res.status(404).end();
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}/`);
